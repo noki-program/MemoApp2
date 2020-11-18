1be5fe498afa41d667eee6edf354070427cd2220
noki-program <noki.g2000@gmail.com>
2020-11-18 20:24:11 +0900

    commit

--- App.js
+++ App.js
@@ -2,43 +2,17 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import Appbar from "./src/components/Appbar";
+import MemoList from "./src/components/MemoList";
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <StatusBar style="auto" />
-        <View style={styles.appbar}>
-          <View>
-            <Text style={styles.appbarTitle}>MEMOT</Text>
-          </View>
-        </View>
 
-        <View style={styles.memoList}>
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2000/11/28</Text>
-          </View>
-
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2000/11/28</Text>
-          </View>
-
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2000/11/28</Text>
-          </View>
-
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2000/11/28</Text>
-          </View>
-
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2000/11/28</Text>
-          </View>
-        </View>
+        <Appbar />
+        <MemoList />
 
         <View style={styles.memoAddButton}>
           <Text style={styles.memoAddButtonTitle}>+</Text>
@@ -54,50 +28,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFFDF6",
     alignItems: "center",
     justifyContent: "center",
-  },
-
-  appbar: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    height: 78,
-    paddingTop: 30,
-    backgroundColor: "#265366",
-    justifyContent: "center",
-    alignItems: "center",
-    shadowOffset: { width: 0, height: 0 },
-    shadowOpacity: 0.5,
-    shadowRadius: 3,
-    zIndex: 10,
-  },
-
-  appbarTitle: {
-    color: "#fff",
-    fontSize: 18,
-  },
-
-  memoList: {
-    width: "100%",
-    flex: 1,
-    paddingTop: 78,
-  },
-
-  memoListItem: {
-    padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ddd",
-    backgroundColor: "#fff",
-  },
-
-  memoTitle: {
-    fontSize: 18,
-    marginBottom: 4,
-  },
-
-  memoDate: {
-    fontSize: 12,
-    color: "#a2a2a2",
   },
 
   memoAddButton: {
